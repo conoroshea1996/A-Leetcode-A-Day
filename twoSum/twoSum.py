a27e3ec99a343623190f5dac5eaba5acf9115086
conoroshea1996 <conoshea12@gmail.com>
2019-07-10 20:49:15 +0100

    update file

--- twoSum/twoSum.py
+++ twoSum/twoSum.py
@@ -1,14 +1,14 @@
 def two_sum(nums, target):
 
-    length = len(nums)  # get length of array
-    for i in range(length):
-        for j in range(length):
+    lenght = len(nums)
 
+    for i in range(lenght):
+        for j in range(lenght):
             value = nums[i] + nums[j]
 
             if value == target and i != j:
-                print([i, j])
-                return [nums[i], nums[j]]
+                return print(' number ' + str(nums[i]) +
+                             ' number ' + str(nums[j]) + ' = ' + str(target))
 
 
-print(two_sum([2, 7, 5, 12, 5], 10))
+print(two_sum([4, 7, 15, 12, 5], 9))
